379ffe97b3fd5cbb594385f722fc4cbd8ecc6f05
MTCMarketing <slapkommerce@gmail.com>
2026-01-21 04:54:21 +0700

    overwrite static-config.js MAIN_CONTENT

--- static-content/static-config.js
+++ static-content/static-config.js
@@ -9,19 +9,10 @@ window.MAIN_CONTENT = {
   "URL_REWRITING": true,
   "DEFAULT_LANGUAGE": "vn",
   "CONTENT": {
-    "VERSION": 1,
-    "UNDER_CONSTRUCTION": false,
-    "ENABLE_EDITOR": false,
-    "ROADMAP_HIDE_RELATED_KEYWORDS": true,
-    "ROADMAP_HIDE_SOCIALS_ON_PROFILE": true,
-    "HIDE_CONTACT_ON_PUBLIC_PROFILE": true,
-    "HIDE_LANDING_VIDEO_SECTION": false,
-    "URL_REWRITING": true,
-    "DEFAULT_LANGUAGE": "vn",
-    "CONTENT": {},
     "meta": {
-      "version": 0,
-      "source": "unknown"
-    }
+      "version": 1,
+      "source": "cms-draft"
+    },
+    "CONTENT": {}
   }
 };
